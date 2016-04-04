b0adcf6b3b56cc1856378fe24b2e0c8f48ad6328
NatalieAdochio <NHAdochio@student.naperville203.org>
2016-04-04 14:12:41 -0500

    4/4

--- BabyNames/WebPage.java
+++ BabyNames/WebPage.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.net.URL;
 import java.util.Scanner;
-
+import java.io.FileNotFound;
 /**
  * Write a description of class WebPage here.
  * 
@@ -11,26 +11,27 @@ import java.util.Scanner;
 public class WebPage
 {
     /** description of instance variable x (add comment for each instance variable) */
-    
+
     /**
      * Default constructor for objects of class WebPage
      */
     public WebPage()
     {
         // initialise instance variables
-        
+
     }
 
-    
     public static void main(String[] args)
     {
         // put your code here
-       String address = "http://horstmann.com/index.html";
-       try{
-           URL pageLocation = new URL(address);
-       Scanner in = new Scanner(pageLocation.openStream());
-    }
-    catch(FileNot
-}
+        String address = "http://horstmann.com/index.html";
+        try{
+            URL pageLocation = new URL(address);
+            Scanner in = new Scanner(pageLocation.openStream());
+        }
+        catch(FileNotFound Exception)
+        {System.out.println("not a file");
+        }
 
+    }
 }
