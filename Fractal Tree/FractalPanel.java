7f85d26480304f55aea7c69ad91a4982a28f8268
NatalieAdochio <itsmenataliehope211@gmail.com>
2016-04-10 15:18:42 -0500

    FABULOSO ARBOL

--- Fractal Tree/FractalPanel.java
+++ Fractal Tree/FractalPanel.java
@@ -1,16 +1,59 @@
-    public void drawFractal(Graphics2D g2, double x, double y, double length, double angle)
+import java.awt.geom.Line2D;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.*;
+import javax.swing.JPanel;
+
+class FractalPanel extends JPanel
+{    
+    private final double BASE_X = 450, BASE_Y = 700, TIP_Y = 450;
+   
+   private final double initialAngle_Radians = 0;
+   
+   private final double initialLength = BASE_Y - TIP_Y;
+   
+   private final double angleOfChange_Radians_CW = Math.PI/4.0;
+   private final double angleOfChange_Radians_CCW = Math.PI/4.0;
+   
+   private final double rateOfChange = 1.5;
+    public FractalPanel ()
     {
-        double fractalAng=Math.toRadians(angle);
-        double findAdj= Math.cos(fractalAng)*length;
-        double findOpp = Math.sin(fractalAng)*length;
-        Line2D.Double fractalLine = new Line2D.Double(x,y,findOpp-x,y+findAdj); 
-        g2.draw(fractalLine);
-        drawFractal(g2,x,y,length-.5, currentAngle+angle);
-        drawFractal(g2,x,y,length-.5,currentAngle-angle);
+      setBackground(Color.black);
+      setPreferredSize(new Dimension(1000, 800));
+    }
+    
+    public void drawFractal(double baseX, double baseY, double tipX, double tipY,
+                            Graphics page, double prevAngle, double prevLength)
+    {
+       if (prevLength <= 1) {
+         page.drawLine((int)BASE_X, (int)BASE_Y, (int)BASE_X, (int)TIP_Y);
+      }
+      else
+      {
+         // 1. Reduce segment length
+         double length = prevLength/rateOfChange;
+         
+         // 2. Set direction equal to its parent's +/- some angle
+         double newAngleCW = prevAngle + angleOfChange_Radians_CW;
+         double newAngleCCW = prevAngle - angleOfChange_Radians_CCW;
+         
+         // 3. Draw a segment by calculating new (x, y) with trig
+         double newXClockwise = tipX - (length * Math.sin(newAngleCW));
+         double newYClockwise = tipY - (length * Math.cos(newAngleCW));
+         page.drawLine((int)tipX, (int)tipY, (int)newXClockwise, (int)newYClockwise);
+     
+         double newXCounterClockwise = tipX - (length * Math.sin(newAngleCCW));
+         double newYCounterClockwise = tipY - (length * Math.cos(newAngleCCW));
+         page.drawLine((int)tipX, (int)tipY, (int)newXCounterClockwise, (int)newYCounterClockwise);
+         drawFractal(tipX, tipY, newXClockwise, newYClockwise, page, newAngleCW, length);
+         drawFractal(tipX, tipY, newXCounterClockwise, newYCounterClockwise, page, newAngleCCW, length);
+        
+      }
     }
     public void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-         super.paintComponent(g);
-        g.setColor(Color.BLACK);
-        drawFractal(g, 400, 500, -90, 9);
+        super.paintComponent(g);
+        g.setColor(Color.WHITE);
+        drawFractal(BASE_X, BASE_Y, BASE_X, TIP_Y, g, initialAngle_Radians, initialLength);
     }
+}
--- Fractal Tree/FractalTree.java
+++ Fractal Tree/FractalTree.java
@@ -1,42 +1,35 @@
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Graphics;
-import javax.swing.JFrame;
-import java.awt.geom.Line2D;
-/**
- * Write a description of class FractalTreeViewer here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class FractalTree extends JFrame
-{
-    double currentAngle =0;
-    double length= 50; 
-    public FractalTree() {
-        super("Fractal Tree");
-        setBounds(100, 100, 800, 600);
-        //setResizable(false);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-    public void drawFractal(Graphics2D g2, double x, double y, double length, double angle)
-    {
-        double fractalAng=Math.toRadians(angle);
-        double findAdj= Math.cos(fractalAng)*length;
-        double findOpp = Math.sin(fractalAng)*length;
-        Line2D.Double fractalLine = new Line2D.Double(x,y,findOpp-x,y+findAdj); 
-        g2.draw(fractalLine);
-        drawFractal(g2,x,y,length-.5, currentAngle+angle);
-        drawFractal(g2,x,y,length-.5,currentAngle-angle);
-    }
-    public void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-         super.paintComponent(g);
-        g.setColor(Color.BLACK);
-        drawFractal(g, 400, 500, -90, 9);
-    }
- 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FractalTree implements ActionListener {
+    private final int WIDTH = 1000;
+    private final int HEIGHT = 800;
+
+    private FractalPanel drawing;
+    private JPanel panel;
+    private JFrame frame;
+    
     public static void main(String[] args) {
-        new FractalTree().setVisible(true);
+        FractalTree viewer = new FractalTree();
+    }
+    
+    public FractalTree () {
+        drawing = new FractalPanel();
+
+        panel = new JPanel();
+        panel.add (drawing);
+
+        frame = new JFrame();
+        frame.setTitle("Fractal Tree");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WIDTH, HEIGHT);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+    
+    public void actionPerformed (ActionEvent event)
+    {
+        frame.repaint();
     }
 }
